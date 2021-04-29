77b9a3fe811dbb0e221e5308cda2d4ab9fa40620
TeaSea33 <iuchs1314@outlook.com>
2021-04-29 20:38:59 +0800

    First GitHub Test

--- TestIO/src/com/chs/IO04/Test03.java
+++ TestIO/src/com/chs/IO04/Test03.java
@@ -9,7 +9,7 @@ import java.io.*;
  * @version: 1.0
  */
 public class Test03 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         //1.先准备输入方向
         //键盘录入：
         InputStream in = System.in;//属于字节流
@@ -19,10 +19,24 @@ public class Test03 {
         BufferedReader  br = new BufferedReader(isr);
 
         //2.再准备输出方向
-        //键盘输出
-        PrintStream out = System.out;
+        //目标文件：文本文档
         //字符流--》字节流
         File f = new File("D:\\FUTURE\\Java\\IDEA_workspace\\TestJavaSE\\TestIO\\src\\com\\chs\\IO04\\Test2.txt");
+        FileWriter fw = new FileWriter(f);
+        BufferedWriter bw = new BufferedWriter(fw);
+
+        //3.开始动作
+        //准备目标文件课在准备输方向
+        String s = br.readLine();
+        while (!s.equals("exit")){
+            bw.write(s);
+            bw.newLine();
+            s = br.readLine();
+        }
+
+        //4.关闭流
+        bw.close();
+        br.close();
 
 
     }
